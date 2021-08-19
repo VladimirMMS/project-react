e6dcb92176d1e71e19cfd21226607aa9fc5a0e56
unknown <vladimirmercado2001@gmail.com>
2021-08-19 08:39:41 -0700

    put router

--- src/components/Navigaction.js
+++ src/components/Navigaction.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import {Navbar, Nav, NavDropdown} from "react-bootstrap"
+import {NavLink} from 'react-router-dom'
+
 
 function Navigaction() {
     return (
-        <div>
-            <h1>Navigation</h1>
-        </div>
+        <Navbar collapseOnSelect expand = "lg" variant = "dark" bg="dark">
+
+            <Navbar.Brand as={NavLink} to="/">Task Manager</Navbar.Brand>
+                
+           
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+
+            <Navbar.Collapse id = "responsive-navbar-nav">
+                <Nav className = "mr-auto">
+                    <Nav.Link as= {NavLink} to="projects">
+                        Projects
+                    </Nav.Link>
+                    <NavDropdown title = "Admin">
+                        <Nav.Item as={NavLink} to="/admin/users">
+                            Users
+                        </Nav.Item>
+                    </NavDropdown>   
+                </Nav>
+                <Nav className = "mx-auto">
+                    <Nav.Link as={NavLink} to="/login">
+                        Login
+                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="/register">
+                        Register
+                    </Nav.Link>
+
+                    <Nav.Link as={NavLink} to="/account">
+                        Account
+                    </Nav.Link>
+                </Nav>
+
+
+            </Navbar.Collapse>
+           
+            
+        </Navbar>
     );
 }
 
